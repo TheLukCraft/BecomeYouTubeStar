4831c1aa25f27dfcb42564b7aa0e3b913065cbfc
TheLukCraft <luk1997r@o2.pl>
2023-06-02 11:17:06 +0200

    Added camera movement and added environment

--- Assets/DeliveryDriver/Delivery.cs
+++ Assets/DeliveryDriver/Delivery.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using UnityEngine;
 
-public class Collision : MonoBehaviour
+public class Delivery : MonoBehaviour
 {
     void OnCollisionEnter2D(Collision2D other)
     {
         UnityEngine.Debug.Log("Ouch!");
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        UnityEngine.Debug.Log("What was that");
     }
 }
