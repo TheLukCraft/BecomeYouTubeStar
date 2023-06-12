358e5816ab3456ffed108c4ca199063ab9835fa3
TheLukCraft <luk1997r@o2.pl>
2023-06-12 10:24:40 +0200

    After collecting the package, change the color of the car

--- Assets/DeliveryDriver/Delivery.cs
+++ Assets/DeliveryDriver/Delivery.cs
@@ -1,11 +1,22 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Diagnostics;
 using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
+    [SerializeField] Color32 hasPackageColor = new Color32(1, 1, 1, 1);
+    [SerializeField] Color32 noPackageColor = new Color32(1, 1, 1, 1);
+    [SerializeField] float destroyDelay = 0.5f;
     bool hasPackage = false;
+
+    SpriteRenderer spriteRenderer;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
     void OnCollisionEnter2D(Collision2D other)
     {
         UnityEngine.Debug.Log("Ouch!");
@@ -13,15 +24,18 @@ public class Delivery : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.tag == "Package")
+        if (other.tag == "Package" && !hasPackage)
         {
             UnityEngine.Debug.Log("Package picked up!");
             hasPackage = true;
+            spriteRenderer.color = hasPackageColor;
+            Destroy(other.gameObject, destroyDelay);
         }
         if (other.tag == "Customer" && hasPackage)
         {
             UnityEngine.Debug.Log("Package delivered!");
             hasPackage = false;
+            spriteRenderer.color = noPackageColor;
         }
     }
 }
