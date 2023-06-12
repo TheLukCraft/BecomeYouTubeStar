8326c9f3e6d27d2c9905f5dbe812d7e18412fdae
TheLukCraft <luk1997r@o2.pl>
2023-06-12 12:10:26 +0200

    Finished mini game delivery driver

--- Assets/DeliveryDriver/Driver.cs
+++ Assets/DeliveryDriver/Driver.cs
@@ -1,16 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
     [SerializeField] float steerSpeed = 300;
     [SerializeField] float moveSpeed = 15;
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
+    [SerializeField] float slowSpeed = 10;
+    [SerializeField] float boostSpeed = 20;
 
     // Update is called once per frame
     void Update()
@@ -19,5 +17,18 @@ public class Driver : MonoBehaviour
         float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
         transform.Rotate(0, 0, -steerAmount);
         transform.Translate(0, moveAmount, 0);
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        moveSpeed = slowSpeed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Boost")
+            moveSpeed = boostSpeed;
+
+
     }
 }
