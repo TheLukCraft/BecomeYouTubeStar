73c46660fcd1d7a45475cb2b8b7fc8d7d3107a6e
TheLukCraft <luk1997r@o2.pl>
2023-06-12 16:59:15 +0200

    fixed crashDetector and finishLine

--- Assets/Snow Boarder/Scripts/CrashDetector.cs
+++ Assets/Snow Boarder/Scripts/CrashDetector.cs
@@ -1,15 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CrashDetector : MonoBehaviour
 {
+    [SerializeField] float loadDelay = 0.5f;
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Ground")
         {
-            UnityEngine.Debug.Log("Ouch!");
+            Invoke(ReloadScene, loadDelay);
         }
     }
-
+    void ReloadScene()
+    {
+        SceneManager.LoadScene(1);
+    }
 }
